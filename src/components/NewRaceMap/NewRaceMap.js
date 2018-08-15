4ece0e62fec76f2b799a08f7b24acca1be80b0dd
Nathan Carroll <ntc238@gmail.com>
2018-08-15 10:08:48 -0500

    toggle modal on map new checkpoints view

--- src/components/NewRaceMap/NewRaceMap.js
+++ src/components/NewRaceMap/NewRaceMap.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-// import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import GoogleMapReact from 'google-map-react';
+import './NewRaceMap.css';
 
 class Target extends Component {
     render(){
+        const showHideClassName = this.props.show ? "modal display-block" : "modal display-none";
         return(
-            // <div height='10px' width='10px' backgroundcolor='red' />
-            <h3>MARKER</h3>
+            <div className={showHideClassName}>
+            <section className="modal-main">
+              {this.props.children}
+              <button onClick={this.props.onClick}>close</button>
+            </section>
+          </div>
         )
     }
 }
@@ -14,24 +19,9 @@ class Target extends Component {
 class NewRaceMap extends Component {
     constructor(props){
         super(props)
-        // this.state = {
-        //     markerList: [
-        //         {
-        //             title: 'i am the best marker',
-        //             name: 'nato3',
-        //             position: {lat: 44.978185, lng: -93.081807}
-        //         },{
-        //             title: 'hello i am the marker',
-        //             name: 'nato',
-        //             position: {lat: 44.978180, lng: -93.081802}
-        //         },{
-        //             title: 'the owrst',
-        //             name: 'potato',
-        //             position: {lat: 44.978181, lng: -93.081803}
-        //         }   
-        // ]
         this.state = {
-            markerList: []
+            markerList: [],
+            showModal: false
         }
     }
 
@@ -40,37 +30,40 @@ class NewRaceMap extends Component {
     }
 
     newCheckpoint = (e) => {
-        // const position = {
-        //     latitude: e.latLng.lat(),
-        //     longitude: e.latLng.lng()
-        // };
-        // console.log(position);
-        // this.setState({
-        //     markerList: [...this.state.markerList, {
-        //         title: 'new marker',
-        //         name: 'nato2',
-        //         position: position
-        //     }]
-        // })
+        if (e.event.target.innerHTML === 'close') return;
         this.setState({
             markerList: [...this.state.markerList, {
                 lat: e.lat,
                 lng: e.lng,
                 text: 'new marker'
-            }]
+            }],
+            showModal: true
         })
         console.log(e);
     }
-    
-    onMarkerClick = (e) => {
-        console.log('click');
+
+    closeModal = (e) => {
+        e.stopPropagation();
+        e.nativeEvent.stopImmediatePropagation();
+        console.log(e.isDefaultPrevented());
+        console.log(e.isPropagationStopped());
         console.log(e);
+        this.setState({
+            showModal: false
+        })
+    }
+    
+    onMarkerClick(e){
+        console.log('clicked on a marker');
+        console.log(this);
     }
 
     render() {
         const allMarkers = this.state.markerList.map((marker, index) => {
             return(
                 <Target
+                    onClick={this.closeModal}
+                    show={this.state.showModal}
                     key={index}
                     {...marker}
                 />
@@ -85,6 +78,7 @@ class NewRaceMap extends Component {
                     defaultZoom={14}
                     onChildClick={this.onMarkerClick}
                     onClick={this.newCheckpoint}
+                    options={{gestureHandling: 'greedy'}}
                 >
                     {/* <Target
                         lat={44.978182}
@@ -99,39 +93,3 @@ class NewRaceMap extends Component {
 };
 
 export default NewRaceMap;
-
-// import React, { Component } from 'react';
-// import GoogleMapReact from 'google-map-react';
-
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-// class SimpleMap extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: 59.95,
-//       lng: 30.33
-//     },
-//     zoom: 11
-//   };
-
-//   render() {
-//     return (
-//       // Important! Always set the container height explicitly
-//       <div style={{ height: '100vh', width: '100%' }}>
-//         <GoogleMapReact
-//           bootstrapURLKeys={{ key:'AIzaSyBfp9E-IfhLx-7zsoW5i79uFXAl63KMJbw'}}
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         >
-//           <AnyReactComponent
-//             lat={59.955413}
-//             lng={30.337844}
-//             text={'Kreyser Avrora'}
-//           />
-//         </GoogleMapReact>
-//       </div>
-//     );
-//   }
-// }
-
-// export default SimpleMap;
