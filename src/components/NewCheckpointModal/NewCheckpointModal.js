93f8c40f0ec7aa14268643ed1715bd5f848e765e
Nathan Carroll <ntc238@gmail.com>
2018-08-22 09:42:34 -0500

    focus on input areas in modal is working right now, no idea what i did to make that happen

--- src/components/NewCheckpointModal/NewCheckpointModal.js
+++ src/components/NewCheckpointModal/NewCheckpointModal.js
@@ -2,31 +2,33 @@ import React, {Component} from 'react';
 import {TextField, Button} from '@material-ui/core';
 
 class NewCheckpointModal extends Component{
+    myAlert = (e) => {
+        console.log('fire');
+        console.log(e.target);
+    }
+
     render(){
         const showHideClassName = this.props.showModal ? "modal display-block" : "modal display-none";
         return(
                 <div className={showHideClassName}>
                 <div className="modal-main">
-                    <div>
                         <TextField
-                            autoFocus
+                            onClick={this.myAlert}
+                            className="modal-input"
                             label="Name"
                             value={this.props.checkpointName} 
                             name="checkpointName" 
-                            placeholder="name" 
+                            // placeholder="name" 
                             onChange={this.props.handleModalChange}
                         />
-                    </div>
-                    <div>
                         <TextField 
+                            className="modal-input"
                             label="Description"
                             value={this.props.checkpointDescription} 
                             name="checkpointDescription" 
-                            placeholder="description" 
+                            // placeholder="description" 
                             onChange={this.props.handleModalChange}
-                            style={{fontsize:'25px'}}
                         />
-                    </div>
                     <Button name="cancel" onClick={this.props.handleModalClick}>Cancel</Button>
                     <Button name="save" onClick={this.props.handleModalClick}>Save</Button>
                 </div>
